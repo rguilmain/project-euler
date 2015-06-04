c84fbc9bd762ade842792ae9f6fc8828ca925ad3
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 16:43:52 -0400

    Add unit tests for euler7.py.

---
+++ euler7_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+import euler7
+
+
+class ProjectEuler7Tests(unittest.TestCase):
+
+  def test_Given(self):
+    self.assertEquals(euler7.get_nth_prime(6), 13)
+
+  def test_Answer(self):
+    self.assertEquals(euler7.get_nth_prime(10001), 104743)
+
+
+if __name__ == "__main__":
+  unittest.main()
