484395e16db24842d65004fdb96587ca7710b5ca
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 10:47:20 -0400

    Add more efficient solution to problem 4.

---
+++ euler4-fast.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def is_palindromic(n):
+  return str(n) == str(n)[::-1]
+
+
+def get_max_palindromic_product(lower_bound, upper_bound):
+  frontier = set([(upper_bound, upper_bound)])
+  while frontier:
+    x, y = max(frontier, key=lambda (x, y) : x * y)
+    if is_palindromic(x * y):
+      return x * y
+    frontier.remove((x, y))
+    if y - 1 >= lower_bound:
+      frontier.add((x, y - 1))
+    if x - 1 >= lower_bound:
+      frontier.add((x - 1, y))
+  print "No palindromic product found!"
+
+
+def main(argv=None):
+  if argv is not None:
+    sys.argv = argv
+
+  print get_max_palindromic_product(100, 999)
+
+
+if __name__ == "__main__":
+  sys.exit(main())
