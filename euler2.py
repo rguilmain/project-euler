f644535639e2ad5a2eb3e8ee9ccf8654eb37175f
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 11:02:45 -0400

    Factor calculations out of main().

--- euler2.py
+++ euler2.py
@@ -1,23 +1,25 @@
 import sys
 
 
-def main(argv=None):
-  if argv is not None:
-    sys.argv = argv
-
+def get_sum_of_even_fibonacci(upper_limit):
   n_2 = 1
   n_1 = 2
-
   result = 2
   while True:
     n = n_2 + n_1
-    if n >= 4000000:
-      print result
-      break
+    if n >= upper_limit:
+      return result
     if n % 2 == 0:
       result += n
     n_2 = n_1
     n_1 = n
+
+
+def main(argv=None):
+  if argv is not None:
+    sys.argv = argv
+
+  print get_sum_of_even_fibonacci(4000000)
 
 
 if __name__ == "__main__":
