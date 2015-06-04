53cb466b638b883d8db6e4f973f2c0f1c95b4163
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 15:06:31 -0400

    Add solution to problem 12.

---
+++ euler12.py
@@ -0,0 +1,58 @@
+import sys
+
+
+def get_nth_triangle_num(n):
+  return n * (n + 1) / 2
+
+
+def range_large(start, stop):
+  """The built-in range function in Python 2.x does not support numbers large
+  enough for this problem, so we make our own. Stolen from euler3.py.
+  """
+  i = start
+  while i < stop:
+    yield i
+    i += 1
+
+
+def get_prime_factorization(n):
+  """Modified from euler3.py.
+  """
+  prime_factors = {}
+  while n != 1:
+    for i in range_large(2, n + 1):
+      if n % i == 0:
+        if i in prime_factors:
+          prime_factors[i] += 1
+        else:
+          prime_factors[i] = 1
+        n /= i
+        break
+  return prime_factors
+
+
+def get_num_divisors(n):
+  prime_factorization = get_prime_factorization(n)
+  num_divisors = 1
+  for factor in prime_factorization:
+    num_divisors *= (prime_factorization[factor] + 1)
+  return num_divisors
+
+
+def get_first_triangle_num_with_over_n_divisors(n):
+  i = 1
+  while get_num_divisors(get_nth_triangle_num(i)) <= n:
+    i += 1
+  return get_nth_triangle_num(i)
+
+
+def main(argv=None):
+  if argv is not None:
+    sys.argv = argv
+
+  n = 500
+  print get_first_triangle_num_with_over_n_divisors(n)
+
+
+if __name__ == "__main__":
+  sys.exit(main())
