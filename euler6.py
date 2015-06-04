29decb8d5043b45df4173021f9b1379517bbfeef
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 16:11:33 -0400

    Factor calculations out of main().

--- euler6.py
+++ euler6.py
@@ -9,12 +9,16 @@ def square_of_sums(n):
   return sum(range(1, n + 1)) * sum(range(1, n + 1))
 
 
+def get_sum_square_difference(n):
+  return square_of_sums(n) - sum_of_squares(n)
+
+
 def main(argv=None):
   if argv is not None:
     sys.argv = argv
 
   n = 100
-  print square_of_sums(n) - sum_of_squares(n)
+  print get_sum_square_difference(n)
 
 
 if __name__ == "__main__":
