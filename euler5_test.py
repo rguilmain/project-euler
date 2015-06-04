65d82c38b9fe9c9a1257f7ce70d6e898a6971d48
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 16:39:52 -0400

    Add unit tests for euler5.py.

---
+++ euler5_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+import euler5
+
+
+class ProjectEuler5Tests(unittest.TestCase):
+
+  def test_Given(self):
+    self.assertEquals(euler5.get_smallest_multiple(10), 2520)
+
+  def test_Answer(self):
+    self.assertEquals(euler5.get_smallest_multiple(20), 232792560)
+
+
+if __name__ == "__main__":
+  unittest.main()
