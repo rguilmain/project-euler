68ca33b5d3896f4154121c4e50e721ca03185203
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 16:11:04 -0400

    Factor calculations out of main().

--- euler3.py
+++ euler3.py
@@ -3,12 +3,16 @@ import sys
 import primality
 
 
+def get_largest_prime_factor(n):
+  return max(primality.get_prime_factorization(n))
+
+
 def main(argv=None):
   if argv is not None:
     sys.argv = argv
 
   n = 600851475143
-  print max(primality.get_prime_factorization(n))
+  print get_largest_prime_factor(n)
 
 
 if __name__ == "__main__":
