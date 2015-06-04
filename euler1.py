b68b6f8533e6d4523eb06c51c6a6ef00e0ace9b5
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 15:58:56 -0400

    Factor calculations out of main().

--- euler1.py
+++ euler1.py
@@ -1,16 +1,20 @@
 import sys
 
 
-def main(argv=None):
-  if argv is not None:
-    sys.argv = argv
-
+def get_sum_of_multiples_of_3_and_5(upper_limit):
   multiples = []
   for i in range(1000):
     if i % 3 == 0 or i % 5 == 0:
       multiples.append(i)
+  return sum(multiples)
 
-  print sum(multiples)
+
+def main(argv=None):
+  if argv is not None:
+    sys.argv = argv
+
+  n = 1000
+  print get_sum_of_multiples_of_3_and_5(n)
 
 
 if __name__ == "__main__":
