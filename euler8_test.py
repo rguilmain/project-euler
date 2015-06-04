3e661f2d972b1443d26f355c19c4279b5e33ba53
Rich Guilmain <richguilmain@gmail.com>
2015-06-04 16:46:04 -0400

    Add unit tests for euler8.py.

---
+++ euler8_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+import euler8
+
+
+class ProjectEuler8Tests(unittest.TestCase):
+
+  def test_Given(self):
+    self.assertEquals(euler8.get_largest_product(4), 5832)
+
+  def test_Answer(self):
+    self.assertEquals(euler8.get_largest_product(13), 23514624000)
+
+
+if __name__ == "__main__":
+  unittest.main()
