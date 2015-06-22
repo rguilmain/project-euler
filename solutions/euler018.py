8c3e75a18aa28939043f3eeb54f767d211e442c0
Rich Guilmain <richguilmain@gmail.com>
2015-06-22 14:05:23 -0400

    Factor duplicate max path sum code out to a utility module.

--- solutions/euler018.py
+++ solutions/euler018.py
@@ -1,19 +1,10 @@
 import sys
 
-
-with open('euler018_triangle.txt', 'r') as f:
-  TRIANGLE = [[int(i) for i in line.split()] for line in f]
+import max_path_sum
 
 
 def get_max_path_sum():
-  """A dynamic programming approach where we (bottom-up and in-place) replace
-  the value of each element in the triangle with that element's utility (it's
-  given value plus the utility of its two children). Then, return the utility
-  of the top element."""
-  for i in range(len(TRIANGLE) - 2, -1, -1):
-    for j in range(len(TRIANGLE[i])):
-      TRIANGLE[i][j] += max(TRIANGLE[i + 1][j], TRIANGLE[i + 1][j + 1])
-  return TRIANGLE[0][0]
+  return max_path_sum.get_max_path_sum('euler018_triangle.txt')
 
 
 def main(argv=None):
---
+++ solutions/max_path_sum.py
@@ -0,0 +1,13 @@
+# Utility function for maximum path sum problems 18 and 67.
+
+def get_max_path_sum(filename):
+  """A dynamic programming approach where we (bottom-up and in-place) replace
+  the value of each element in the given triangle with that element's utility
+  (it's given value plus the utility of its two children). Then, return the
+  utility of the top element."""
+  with open(filename, 'r') as f:
+    triangle = [[int(i) for i in line.split()] for line in f]
+  for i in range(len(triangle) - 2, -1, -1):
+    for j in range(len(triangle[i])):
+      triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+  return triangle[0][0]
