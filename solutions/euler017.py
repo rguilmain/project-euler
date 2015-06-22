1eacb186bf6f1779779c3e1f1dce357fb3c4a016
Rich Guilmain <richguilmain@gmail.com>
2015-06-22 17:24:22 -0400

    Whitespace nits.

--- solutions/euler017.py
+++ solutions/euler017.py
@@ -1,13 +1,13 @@
 import sys
 
 
-ones  = {0: "", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six",
-         7: "seven", 8: "eight", 9: "nine"}
+ones = {0: "", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six",
+        7: "seven", 8: "eight", 9: "nine"}
 teens = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen",
          15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen",
          19: "nineteen"}
-tens  = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty",
-         7: "seventy", 8: "eighty", 9: "ninety"}
+tens = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 6: "sixty",
+        7: "seventy", 8: "eighty", 9: "ninety"}
 
 
 def get_num_letters(number):
