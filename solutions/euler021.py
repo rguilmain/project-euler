cce459af325f4302e658c4b8315bf4dfc1522fec
Rich Guilmain <richguilmain@gmail.com>
2015-06-27 01:29:08 -0400

    Add solution for problem 21.

---
+++ solutions/euler021.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def d(n):
+  return sum([i for i in range(1, int(n / 2) + 1) if n % i == 0])
+
+
+def get_sum_of_amicable_numbers(upper_bound):
+  return sum([i for i in range(upper_bound) if i == d(d(i)) and i != d(i)])
+
+
+def main(argv=None):
+  if argv is not None:
+    sys.argv = argv
+
+  upper_bound = 10000
+  print get_sum_of_amicable_numbers(upper_bound)
+
+
+if __name__ == "__main__":
+  sys.exit(main())
