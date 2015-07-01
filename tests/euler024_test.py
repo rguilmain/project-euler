25bef7577a65355e55dc6beed1659257e1703c89
Rich Guilmain <richguilmain@gmail.com>
2015-07-01 14:46:21 -0400

    Add unit tests for problem 24.

---
+++ tests/euler024_test.py
@@ -0,0 +1,20 @@
+import math
+import unittest
+
+from solutions import euler024
+
+
+class ProjectEuler024Tests(unittest.TestCase):
+
+  def test_Given(self):
+    self.assertEquals(euler024.get_nth_permutation(1, range(3)), "012")
+    self.assertEquals(euler024.get_nth_permutation(3, range(3)), "102")
+    self.assertEquals(euler024.get_nth_permutation(6, range(3)), "210")
+
+  def test_Answer(self):
+    self.assertEquals(
+      euler024.get_nth_permutation(1000000, range(10)), "2783915460")
+
+
+if __name__ == "__main__":
+  unittest.main()
